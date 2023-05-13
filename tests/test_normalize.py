fb95fd5a0061becd390722da9ccdd7eff987f22a
Vladimir Larkin <vd-larkin@ya.ru>
2023-05-13 17:33:46 +0300

    Update tests

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -5,5 +5,17 @@ import tsnorm
 
 def test_stress():
     normalizer = tsnorm.Normalizer(stress_mark="+", stress_mark_pos="before")
-    stressed_text = normalizer("Волки, ежики и хомяки. Вот они, обитатели леса.")
-    assert stressed_text == "В+олки, +ёжики и хомяк+и. Вот он+и, обит+атели л+еса."
+    stressed_text = normalizer("Волки, ежи и хомяки. Вот они, обитатели леса.")
+    assert stressed_text == "В+олки, еж+и и хомяк+и. Вот он+и, обит+атели л+еса."
+
+
+def test_stress_yo():
+    normalizer = tsnorm.Normalizer(stress_mark="+", stress_mark_pos="before", stress_yo=True)
+    stressed_text = normalizer("Как хорошо-то, ёмана!")
+    assert stressed_text == "Как хорош+о-то, +ёмана!"
+
+
+def test_stress_monosyllabic():
+    normalizer = tsnorm.Normalizer(stress_mark="+", stress_mark_pos="before", stress_monosyllabic=True)
+    stressed_text = normalizer("Как хорошо, как привольно!")
+    assert stressed_text == "К+ак хорош+о, к+ак прив+ольно!"
