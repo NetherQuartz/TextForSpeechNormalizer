46933f1c2b125ec6d2ae164b9c4dea3c5cdc3f5e
Vladimir Lar’kin <vladimir@larkin.one>
2024-12-19 03:11:31 +0300

    Update version

--- tsnorm/__init__.py
+++ tsnorm/__init__.py
@@ -3,7 +3,7 @@ from .types import CustomDictionary, WordForm, WordFormTags, Lemma, LemmaPOS
 
 from .spacy_model import download_model
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 
 download_model()
